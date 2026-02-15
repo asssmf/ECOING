005c15b0e7333bcb07126391d99efaa86e5d8730
asssmf <wafertaco@gmail.com>
2026-02-15 12:56:39 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,28 +9,13 @@ export default {
       animation: {
         'shake-crazy': 'shake 0.5s cubic-bezier(.36,.07,.19,.97) both',
         'spin-slow': 'spin 3s linear infinite',
-        'slide-up': 'slideUp 0.3s ease-out',
-        'slide-down': 'slideDown 0.3s ease-out',
-        'fadeIn': 'fadeIn 0.5s ease-out',
       },
       keyframes: {
         shake: {
           '10%, 90%': { transform: 'translate3d(-1px, 0, 0)' },
           '20%, 80%': { transform: 'translate3d(2px, 0, 0)' },
           '30%, 50%, 70%': { transform: 'translate3d(-4px, 0, 0)' },
-          '40%, 60%': { transform: 'translate3d(4px, 0, 0)' }
-        },
-        slideUp: {
-          '0%': { transform: 'translateY(100%)' },
-          '100%': { transform: 'translateY(0)' },
-        },
-        slideDown: {
-          '0%': { transform: 'translateY(-100%)' },
-          '100%': { transform: 'translateY(0)' },
-        },
-        fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
+          '40%, 60%': { transform: 'translate3d(4px, 0, 0)' },
         }
       }
     },
